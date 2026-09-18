2401f8b97e33dc11109ffb9e4001979ff1fcbb33
Polaka Pavan Kumar <129263679+Pavan779786@users.noreply.github.com>
2026-09-18 12:51:05 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,8 @@
+name="virat"
+age=24
+height=7.6
+
+print(name)
+print(age)
+print(height)
+
---
+++ p18.py
@@ -0,0 +1,7 @@
+student_name=input("Enter the student name")
+student_branch=input("Enter the branch")
+student_fav_subject=input("Entert your fav subject")
+
+
+print(student_name,student_branch,student_fav_subject)
+
